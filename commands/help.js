47224e3c0be81d428cf8770470a3b98f49ff7020
bata_butter <113527212+batabutter@users.noreply.github.com>
2025-07-25 06:43:02 -0400

    fixed some style issues

--- commands/help.js
+++ commands/help.js
@@ -12,13 +12,13 @@ module.exports = {
             .setTitle(`List of commands for **Norbot:**`)
             .setDescription(`
                 \`/help\` : Shows the list of available commands\n
-                \`/play [input]\` Play an mp3 by searching YouTube with either a URL or a query
+                \`/play [input]\` Play an MP3 by searching YouTube with either a URL or a query
                 \`/skip\` Skips the current track
                 \`/loop\` Loops the current track\n
                 \`/loopqueue\` Loops the entire queue
                 \`/queue\` Shows the current songs in the queue\n\`/clear\` Clears the queue
                 \`/remove [input]\` Removes a track from the queue at the given position\n 
-                **Please let me know if there are any bugs that arise!**",`)
+                **Please let me know if there are any bugs!**`)
             .setColor(0x06402B);
 
         return interaction.reply({ embeds: [help] });
